3800c9679958c239d1e22616cc39ff5da36b32b2
wisarut <yeast72@gmail.com>
2019-07-14 23:05:14 +0700

    update bookmarks list styled

--- src/components/Bookmark/Bookmark.js
+++ src/components/Bookmark/Bookmark.js
@@ -1,11 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Bookmark = ({ id, name, url }) => (
-  <div>
-    <a href={url} target="_blank">
-      {name}
-    </a>
-  </div>
+  <BookmarkInfo>
+    <FontAwesomeIcon icon={faBookmark} />
+    <BookmarkName href={url} target="_blank">
+      {name} <BookmarkURL>- {url}</BookmarkURL>
+    </BookmarkName>
+  </BookmarkInfo>
 );
 
 export default Bookmark;
+
+const BookmarkInfo = styled.div`
+  display: inline-block;
+  color: black;
+`;
+
+const BookmarkName = styled.a`
+  text-decoration: none;
+  color: black;
+  margin-left: 10px;
+`;
+
+const BookmarkURL = styled.span`
+  color: gray;
+`;
--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -1,15 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
 
 const Folder = ({ name, id }) => {
   return (
-    <Link to={id}>
+    <FolderInfo to={id}>
       <FontAwesomeIcon icon={faFolder} />
-      {name}
-    </Link>
+      <FolderName>{name}</FolderName>
+    </FolderInfo>
   );
 };
 
 export default Folder;
+
+const FolderName = styled.span`
+  margin-left: 10px;
+  text-decoration: none;
+  color: black;
+`;
+
+const FolderInfo = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
